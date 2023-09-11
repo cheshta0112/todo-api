7bc0204789a6e7fee3ddbefd14409e177830135c
Cheshta Pal <cheshtapal07@gmail.com>
2023-09-11 18:46:01 +0530

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const todoRoutes=require("./todoRoutes");
+const cors = require("cors");
+const todoRoutes = require("./todoRoutes");
 
 const server = express();
+
+const corsOptions = {
+  origin: "http://localhost:4100",
+  methods: ["GET", "POST"],
+};
+
+server.use(cors(corsOptions)); // Use the cors middleware with your options
+
 server.listen(4100);
 
 server.use("/api/todos/", todoRoutes.router);
 
-server.get("/", (req, res)=>{
-    res.send("Welcome to Utility api");
+server.get("/", (req, res) => {
+  res.send("Welcome to Utility api");
 });
+
 console.log("Server is listening at 4100");
